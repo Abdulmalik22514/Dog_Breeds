b7ab27562d5769819718ede7d5b88821de40bd32
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-05-15 08:23:17 +0100

    chore: Complete TabItem component

---
+++ src/components/TabItems.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import TabIcon from 'react-native-vector-icons/Ionicons';
+import {COLORS} from '../constants/colors';
+
+const TabItem = ({focused, icon, label}) => {
+  return (
+    <View style={styles.tabContainer}>
+      <TabIcon
+        name={icon}
+        size={23}
+        color={focused ? COLORS.orange : COLORS.grey}
+      />
+      <Text
+        style={[
+          styles.tabIconLabel,
+          {color: focused ? COLORS.orange : COLORS.grey},
+        ]}>
+        {label}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabIconLabel: {
+    fontSize: 20,
+  },
+});
+
+export default TabItem;
