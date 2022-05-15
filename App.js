03a4d73a3dc6ff9c9153c9cad94ea975bbaa0abc
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-05-15 08:22:00 +0100

    chore: Refactor

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-import Navigator from './src/navigations/StackNav';
+import Tabs from './src/navigations/TabNav';
 import {persistor, store} from './src/store/breedStore/store';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Navigator />
+        <Tabs />
       </PersistGate>
     </Provider>
   );
