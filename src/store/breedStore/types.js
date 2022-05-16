93c0765a7d13e3ec6d58db4775b6b10101ea448e
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-05-16 23:14:35 +0100

    chore: Add breed details actions

--- src/store/breedStore/types.js
+++ src/store/breedStore/types.js
@@ -2,6 +2,8 @@ export const GET_DOGS_BEGIN = 'GET_DOGS_BEGIN';
 export const GET_DOGS_SUCCESS = 'GET_DOGS_SUCCESS';
 export const GET_DOGS_FAILURE = 'GET_DOGS_FAILURE';
 export const ADD_TO_FAVORITES = 'ADD_TO_FAVORITES';
+export const GET_BREED_DETAILS = 'GET_BREED_DETAILS';
+export const GET_BREED_DETAILS_FAILURE = 'GET_BREED_DETAILS_FAILURE';
 
 export const getDogsSuccess = dogs => ({
   type: GET_DOGS_SUCCESS,
@@ -10,6 +12,16 @@ export const getDogsSuccess = dogs => ({
 
 export const getDogsFailure = error => ({
   type: GET_DOGS_FAILURE,
+  payload: error,
+});
+
+export const getBreedImageSuccess = image => ({
+  type: GET_BREED_DETAILS,
+  payload: image,
+});
+
+export const getBreedImageFailure = error => ({
+  type: GET_BREED_DETAILS_FAILURE,
   payload: error,
 });
 
