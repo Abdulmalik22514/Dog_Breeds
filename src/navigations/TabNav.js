2d36ca08879217142840d6ae4af8ddf2de98aee5
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-05-18 01:28:17 +0100

    feat: Set up navigations

--- src/navigations/TabNav.js
+++ src/navigations/TabNav.js
@@ -1,48 +1,58 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabItem from '../components/TabItems';
 import Profile from '../screens/Profile';
-import {NavigationContainer} from '@react-navigation/native';
-import MyStack from './StackNav';
+import AllBreeds from '../screens/AllBreeds';
+import DogDetails from '../screens/DetailsScreen';
 
 const Tab = createBottomTabNavigator();
 
+const Stack = createNativeStackNavigator();
+
+export const MyStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="StackBreeds" component={AllBreeds} />
+      <Stack.Screen name="DogDetails" component={DogDetails} />
+    </Stack.Navigator>
+  );
+};
+
 const Tabs = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="AllBreeds"
-        screenOptions={{
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            backgroundColor: '#E3E3E3',
-            height: 60,
+    <Tab.Navigator
+      initialRouteName="AllBreeds"
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: '#E3E3E3',
+          height: 60,
+        },
+        headerShown: false,
+      }}>
+      <Tab.Screen
+        name="AllBreeds"
+        component={MyStack}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <TabItem focused={focused} icon="folder" label={'Home'} />;
           },
-          headerShown: false,
-        }}>
-        <Tab.Screen
-          name="AllBreeds"
-          component={MyStack}
-          options={{
-            tabBarIcon: ({focused}) => {
-              return <TabItem focused={focused} icon="folder" label={'Home'} />;
-            },
-          }}
-        />
+        }}
+      />
 
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({focused}) => {
-              return (
-                <TabItem focused={focused} icon="person" label={'Profile'} />
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <TabItem focused={focused} icon="person" label={'Profile'} />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
